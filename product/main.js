0d353cd4d61847fa8a20f6ac8c7bd1517e37cd70
Joy Neop <joyneop@gmail.com>
2014-11-04 13:13:54 +0000

    theme updates

---
+++ product/main.js
@@ -0,0 +1,25 @@
+function getCurrentChapterNumber() {
+	return Number(window.location.href.match(/chapter-[0-9]*\.html/)[0].match(/[0-9]+/)[0]);
+};
+
+function wget(url) {
+	var x = new XMLHttpRequest();
+	x.open("GET", url, false);
+	x.send();
+	return x.responseText;
+};
+
+function json(url) {
+	return JSON.parse(wget(url));
+};
+
+function detectEdgesOfBook() {
+	var chapters = wget("chapters.json");
+	if (getCurrentChapterNumber() == 0) {
+		document.getElementById("nav-prev-chapter").remove();
+		document.getElementById("nav-next-chapter").href = "chapter-" + (getCurrentChapterNumber() + 1) + "";
+	} else if (getCurrentChapterNumber() == chapters.length - 1) {
+		document.getElementById("nav-next-chapter").remove();
+		document.getElementById("nav-prev-chapter").href = "chapter-" + (getCurrentChapterNumber() - 1) + "";
+	};
+};
